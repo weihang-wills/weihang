fd2a3ec254091edf012d93fc4b895857812ce73c
weihang-wills <weihang-wills@users.noreply.github.com>
2018-05-07 14:35:09 +0800

    unity

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # unity
 
+##一些关于unity的方法记录
+
+- animator 动画状态机
+- active 物体可见
+- scene 进入场景
+
---
+++ active/test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class test : MonoBehaviour
+{
+
+    public GameObject Gd;
+    public GameObject Fd;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Gd = GameObject.Find("Canvas 2");
+        Fd = GameObject.Find("Canvas");
+
+
+        Gd.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Onclick()
+    {
+
+        Gd.SetActive(true);
+        Fd.SetActive(false);
+
+    }
+
+    void Scene()
+    {
+
+        SceneManager.LoadScene(5);
+
+
+    }
+}
