8aa3ae92c525efca8dabce3a491278abb9f07625
weihang-wills <weihang-wills@users.noreply.github.com>
2018-05-07 13:01:42 +0800

    First commit.

---
+++ Mo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mo : MonoBehaviour {
+
+    public Animator hahaha;
+
+
+    // Use this for initialization
+    void Start() {
+       
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void onclick()
+    {
+        hahaha.SetBool("dianji", true);
+
+    }
+}
---
+++ mo2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mo2 : MonoBehaviour
+{
+
+    public Animator hahaha;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+          hahaha.SetBool("dianji", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            hahaha.SetBool("haha", true);
+
+        }
+        
+
+
+        
+
+    }
+}
+
+
+		
+	
+
