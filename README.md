9ee1f53d6154f648b31e94e16835368bc3493095
weihang-wills <weihang-wills@users.noreply.github.com>
2018-06-06 18:55:13 +0800

    attack

--- README.md
+++ README.md
@@ -6,4 +6,6 @@
 - animator 动画状态机
 - active 物体可见
 - scene 进入场景
+- control 人物控制，速度，加力，碰撞监测
+- attack 攻击战斗相关
 
---
+++ attack/blood.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class blood : MonoBehaviour {
+    //血条扣减和扣血淡出动画
+
+    public Slider slider;
+    private uint bloodline;
+    public GameObject blooddecline;
+    
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        bloodline = 100;
+        blooddecline.SetActive(false);
+        
+
+        
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        slider.value = bloodline/100f;
+		
+	}
+    public void Onclickbutton()
+    {
+        if (bloodline > 0)
+        {
+            bloodline = bloodline - 10;
+            blooddecline.SetActive(true);
+            Component[] comps = blooddecline.GetComponentsInChildren<Component>();//局部变量
+            foreach (Component c in comps)
+                            {
+                if (c is Graphic)
+                {
+                    (c as Graphic).CrossFadeAlpha(0, 1, true); //只针对graphic才能有这个淡出效果，（目标alpha，淡出时间，bool)
+                }
+                
+            }
+            
+
+        }
+
+        
+
+    }
+}
