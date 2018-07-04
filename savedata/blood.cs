866b080026a92f4bac1dc49d6b324af2512e5dd4
weihang-wills <weihang-wills@users.noreply.github.com>
2018-07-04 13:42:40 +0800

    save

---
+++ savedata/blood.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LitJson;
+
+public class blood : MonoBehaviour {
+    //血条扣减和扣血淡出动画
+
+    public Slider slider;
+    private int bloodline;
+    public GameObject blooddecline;
+    
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        bloodline = 100;
+        blooddecline.SetActive(false);
+        
+
+        
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        slider.value = bloodline/100f;
+		
+	}
+    public void savedata()
+    {
+        persondata person = new persondata();
+        person.shengming = bloodline;//存值进去
+        person.lingqi = 50;
+        save.savedate(person);
+    }
+    public void Loaddatas()
+    {
+        
+        save save1 = new save();//实例化一个save类用来取本地的值
+        save1.Loaddata();        
+        bloodline = save1.person.shengming;//取此时的persondata类里面的值
+        Debug.Log(bloodline);
+    }
+    public void Onclickbutton()
+    {
+        if (bloodline > 0)
+        {
+            bloodline = bloodline - 10;
+            blooddecline.SetActive(true);
+            Component[] comps = blooddecline.GetComponentsInChildren<Component>();//局部变量
+            foreach (Component c in comps)
+                            {
+                if (c is Graphic)
+                {
+                    (c as Graphic).CrossFadeAlpha(0, 1, true); //只针对graphic才能有这个淡出效果，（目标alpha，淡出时间，bool)
+                }
+                
+            }
+            
+
+        }
+
+        
+
+    }
+}
---
+++ savedata/save.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using LitJson;
+
+public class persondata
+{     //数据列表类
+    public int shengming;
+    public int lingqi;
+
+}
+public class save{
+
+
+
+    public persondata person = new persondata();
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Loaddata()
+    {
+        string filepath = Application.dataPath + @"/persondata/persondata.json";
+        Debug.Log(Application.dataPath + @"/persondata/persondata.json");
+
+        if (!File.Exists(filepath))
+        {
+            Debug.Log("no file");
+            return;
+        }
+
+        StreamReader sr = new StreamReader(filepath);
+
+        
+        string dataset = sr.ReadToEnd();//取值
+        Debug.Log(dataset);
+
+        person = JsonMapper.ToObject<persondata>(dataset);//toobject赋值给persondata类
+        
+        
+        
+
+    }
+    public static void savedate(persondata data)
+    {
+        string filepath = Application.dataPath + @"/persondata/persondata.json";
+        Debug.Log(Application.dataPath + @"/persondata/persondata.json");
+
+        string json = JsonMapper.ToJson(data);//persondata类传值进来，转为json数据保存
+        Debug.Log(json);
+
+        FileInfo file = new FileInfo(filepath);
+        StreamWriter sw = file.CreateText();
+        sw.WriteLine(json);
+        sw.Close();
+        sw.Dispose();
+        AssetDatabase.Refresh();
+
+    }
+    
+}
